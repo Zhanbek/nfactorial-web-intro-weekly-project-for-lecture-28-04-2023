d258bd2b121c42a3705557387d29c91e97c5d4f4
Turaneed <zhanbek.sarsenov@gmail.com>
2023-05-18 01:25:43 +0600

    Adds some styles for ActionItem's elements

--- todolist/src/Components/ActionRecord/ActionRecord.js
+++ todolist/src/Components/ActionRecord/ActionRecord.js
@@ -1,11 +1,19 @@
 import './ActionRecord.css'
+import './ActionRecord-outline-temp.css'
 
 export default function ActionRecord({actionItem}) {
     return (
-        <li class = "Actions-list-item">
-            <button class = "callModalWindow"></button>
-            <input type = "checkbox"></input>
-            <span>{actionItem.name}</span>
+        <li className = "Actions-list-item">
+            
+            <button className = "Call-modal-window">
+                <img className = "Call-modal-window-img" src = "./CallModalWindow.svg"></img>
+            </button>
+            
+            <span className = "Action-checkbox-container">
+                <input type = "checkbox" class = "Action-checkbox"></input>""
+            </span>
+            
+            <span className = "Action-title">{actionItem.name}</span>
         </li>
     );
 }
