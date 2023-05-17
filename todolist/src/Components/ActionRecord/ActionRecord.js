b6f9f3a34a8f8b19ffe22734d642f5f6a0aa64ef
Turaneed <zhanbek.sarsenov@gmail.com>
2023-05-17 23:33:10 +0600

    Renames 'ToDo' status to 'To Do'

---
+++ todolist/src/Components/ActionRecord/ActionRecord.js
@@ -0,0 +1,8 @@
+import './ActionRecord.css'
+import { useState } from "react";
+
+import { v4 as uuidv4 } from "uuid";
+
+default export ActionRecord() {
+    return <div class = "ActionRecord"></div>
+}
--- todolist/src/Components/Main/Main.js
+++ todolist/src/Components/Main/Main.js
@@ -7,11 +7,11 @@ import { v4 as uuidv4 } from "uuid";
 export default function Main() {
     const INIT_ACTIONS_LIST = [
         {id : uuidv4(), name : "Go to the grocery store",       status : "Done"},
-        {id : uuidv4(), name : "Сook food",                     status : "ToDo"},
-        {id : uuidv4(), name : "Path the exam",                 status : "ToDo"},
-        {id : uuidv4(), name : "Wash the dishes",               status : "ToDo"},
+        {id : uuidv4(), name : "Сook food",                     status : "To Do"},
+        {id : uuidv4(), name : "Path the exam",                 status : "To Do"},
+        {id : uuidv4(), name : "Wash the dishes",               status : "To Do"},
         {id : uuidv4(), name : "Go to karaoke",                 status : "Trash"},
-        {id : uuidv4(), name : "Read a lecture",                status : "ToDo"},
+        {id : uuidv4(), name : "Read a lecture",                status : "To Do"},
         {id : uuidv4(), name : "Сheck homework assignments",    status : "Done"}
     ];
     
@@ -39,7 +39,7 @@ export default function Main() {
                     </button>
                 </li>
                  <li className = "buttons-list-item">
-                    <button className = "Set-display-mode" onClick = {() => handleTypeStatus('ToDo')}>
+                    <button className = "Set-display-mode" onClick = {() => handleTypeStatus('To Do')}>
                         To Do
                     </button>
                 </li>
@@ -55,17 +55,19 @@ export default function Main() {
                 </li>
             </ul>    
         
-            <p class = "Display-status">statusValue</p>
+            <p class = "Display-status">{statusValue}</p>
            
             <hr color="gray" size="2" style = {{ marginBottom: '24px' }} />
 
-            {
-                filterActions(statusValue).map(
-                    (item) => {
-                        return <p>{item.name}</p>
-                    }
-                )
-            }
+            <ul class = "Actions-list">
+                {
+                    filterActions(statusValue).map(
+                        (item) => {
+                            return <p>{item.name}</p>
+                        }
+                    )
+                }
+            </ul>    
 
         </div>
     );
